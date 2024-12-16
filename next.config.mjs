3616f0eb8f70b8dfe50417d2694c2368a506423c
cslomarfaruk <cslomarfaruk@gmail.com>
2024-12-16 10:22:59 +0600

    api done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/admission.sec.ac.bd/' : '',
+  basePath: isProd ? '/admission.sec.ac.bd' : '',
+  output: 'export'
+};
 
 export default nextConfig;
